a5d5e44b2d6c10b4c442ca10dc748c9e5ec6f805
Róisín Grannell <r.grannell2@gmail.com>
2022-11-25 23:13:00 +0000

    added service worker

---
+++ static/sw.js
@@ -0,0 +1,21 @@
+const CSS_DIR = "css";
+
+const resources = [
+  `/style.css`
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("sw-cache").then(function (cache) {
+      return Promise.all(resources.map((resource) => cache.add(resource)));
+    }),
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    }),
+  );
+});
