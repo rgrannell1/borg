745e43a3f7049770cb55b174c03c1d23c76672b4
Róisín Grannell <r.grannell2@gmail.com>
2023-04-29 22:28:21 +0100

    improved support for small screens

--- static/sw.js
+++ static/sw.js
@@ -16,7 +16,46 @@ self.addEventListener("install", function (event) {
   );
 });
 
+async function getWindowURL(event) {
+  const clientId = event.clientId;
+
+  if (clientId && clients.get) {
+    const client = await clients.get(clientId);
+    return new URL(client.url);
+  } else {
+    const allClients = await clients.matchAll({type: 'window'});
+    const filtered = allClients.filter(client => client.id === clientId);
+
+    if (filtered.length > 0) {
+      return new URL(filtered[0].url);
+    }
+  }
+}
+
+async function receiveShare(event) {
+  const formData = await event.request.formData();
+
+  const windowUrl = await getWindowURL(event);
+  const redirectUrl = '/'
+
+  event.ports[0].postMessage({
+    type: 'share-target',
+    data: {
+      url: formData.length()
+    }
+  });
+
+
+  return Response.redirect('/', 303);
+}
+
 self.addEventListener("fetch", function (event) {
+  // -- intercept web-shares to Borg
+  if (event.request.method === 'POST' && event.request?.url?.pathname === '/_web-share-target') {
+    return receiveShare(event);
+  }
+
+  // respond with any cached results
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
